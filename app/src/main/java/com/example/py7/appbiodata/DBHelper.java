41d8f53e3f3cb21bd145e88f33bd95658c7f7f71
BENJA(-) <101829863+EvanBenyamin@users.noreply.github.com>
2022-12-01 01:11:44 +0700

    commit to main

---
+++ app/src/main/java/com/example/py7/appbiodata/DBHelper.java
@@ -0,0 +1,178 @@
+package com.example.py7.appbiodata;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DBHelper extends SQLiteOpenHelper {
+
+    public static final String database_name = "db_reciapp";
+    public static final String table_name = "tabel_resep";
+
+    public static final String row_id = "_id";
+    public static final String row_nama = "Nama";
+    public static final String row_jenis_hidangan = "TempatLahir";
+    public static final String row_bahan = "Alamat";
+    public static final String row_langkah="Nomor";//Tambahan
+    public static final String row_foto = "Foto";
+
+
+    //TABLE NAME
+    public static final String TABLE_USERS = "users";
+
+    //TABLE USERS COLUMNS
+    //ID COLUMN @primaryKey
+    public static final String KEY_ID = "id";
+
+    //COLUMN user name
+    public static final String KEY_USER_NAME = "username";
+
+    //COLUMN email
+    public static final String KEY_EMAIL = "email";
+
+    //COLUMN password
+    public static final String KEY_PASSWORD = "password";
+
+    //SQL for creating users table
+    public static final String SQL_TABLE_USERS = " CREATE TABLE " + TABLE_USERS
+            + " ( "
+            + KEY_ID + " INTEGER PRIMARY KEY, "
+            + KEY_USER_NAME + " TEXT, "
+            + KEY_EMAIL + " TEXT, "
+            + KEY_PASSWORD + " TEXT"
+            + " ) ";
+
+    private SQLiteDatabase db;
+
+    public DBHelper(Context context) {
+        super(context, database_name, null, 2);
+        db = getWritableDatabase();
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String query = "CREATE TABLE " + table_name + "(" + row_id + " INTEGER PRIMARY KEY AUTOINCREMENT," +
+                " TEXT, " + row_nama + " TEXT, "
+                + row_jenis_hidangan + " TEXT, "
+                + row_bahan + " TEXT, "+ row_langkah +" TEXT,"
+                + row_foto +" TEXT)";
+        db.execSQL(SQL_TABLE_USERS);
+        db.execSQL(query);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int i, int x) {
+        db.execSQL("DROP TABLE IF EXISTS " + table_name);
+        db.execSQL(" DROP TABLE IF EXISTS " + TABLE_USERS);
+    }
+
+    public void addUser(User user) {
+
+        //get writable database
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        //create content values to t
+        ContentValues values = new ContentValues();
+
+        //Put username in  @values
+        values.put(KEY_USER_NAME, user.userName);
+
+        //Put email in  @values
+        values.put(KEY_EMAIL, user.email);
+
+        //Put password in  @values
+        values.put(KEY_PASSWORD, user.password);
+
+        // insert row
+        long todo_id = db.insert(TABLE_USERS, null, values);
+    }
+
+    public User Authenticate(User user) {
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor cursor = db.query(TABLE_USERS,// Selecting Table
+                new String[]{KEY_ID, KEY_USER_NAME, KEY_EMAIL, KEY_PASSWORD},//Selecting columns want to query
+                KEY_EMAIL + "=?",
+                new String[]{user.email},//Where clause
+                null, null, null);
+
+        if (cursor != null && cursor.moveToFirst()&& cursor.getCount()>0) {
+            //if cursor has value then in user database there is user associated with this given email
+            User user1 = new User(cursor.getString(0), cursor.getString(1), cursor.getString(2), cursor.getString(3));
+
+            //Match both passwords check they are same or not
+            if (user.password.equalsIgnoreCase(user1.password)) {
+                return user1;
+            }
+        }
+
+        //if user password does not matches or there is no record with that email then return @false
+        return null;
+    }
+
+    public boolean isEmailExists(String email) {
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor cursor = db.query(TABLE_USERS,// Selecting Table
+                new String[]{KEY_ID, KEY_USER_NAME, KEY_EMAIL, KEY_PASSWORD},//Selecting columns want to query
+                KEY_EMAIL + "=?",
+                new String[]{email},//Where clause
+                null, null, null);
+
+        if (cursor != null && cursor.moveToFirst()&& cursor.getCount()>0) {
+            //if cursor has value then in user database there is user associated with this given email so return true
+            return true;
+        }
+
+        //if email does not exist return false
+        return false;
+    }
+
+    public Cursor checkUser(String user, String passw) {
+        SQLiteDatabase db = getReadableDatabase();
+
+        Cursor cur = db.rawQuery("SELECT * FROM " + SQL_TABLE_USERS + " WHERE " + KEY_USER_NAME + " = " + user + " AND " + KEY_PASSWORD + " = " + passw, null);
+
+        return cur;
+    }
+
+    //Get All SQLite Data
+    public Cursor allData(){
+        db = getWritableDatabase();
+        Cursor cur = db.rawQuery("SELECT * FROM " + table_name , null);
+        return cur;
+    }
+
+    //Get 1 Data By ID
+    public Cursor oneData(Long id){
+        db = getWritableDatabase();
+        Cursor cur = db.rawQuery("SELECT * FROM " + table_name + " WHERE " + row_id + "=" + id, null);
+        return cur;
+    }
+
+    public Cursor Datain(String emaili){
+        db = getWritableDatabase();
+        Cursor cur = db.rawQuery("select * from "+ TABLE_USERS +" where "+KEY_EMAIL+"= ?", new String[]{emaili});
+        return cur;
+    }
+
+    //Insert Data to Database
+    public void insertData(ContentValues values){
+        db.insert(table_name, null, values);
+    }
+
+    //Update Data
+    public void updateData(ContentValues values, long id){
+        db.update(table_name, values, row_id + "=" + id, null);
+    }
+
+    //Delete Data
+    public void deleteData(long id){
+        db.delete(table_name, row_id + "=" + id, null);
+    }
+
+
+
+}
+
+
---
+++ app/src/main/java/com/example/py7/appbiodata/EditActivity.java
@@ -0,0 +1,178 @@
+package com.example.py7.appbiodata;
+
+import android.Manifest;
+import android.annotation.TargetApi;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.DatePickerDialog;
+import android.content.ContentValues;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Build;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import com.blogspot.atifsoftwares.circularimageview.CircularImageView;
+import com.theartofdev.edmodo.cropper.CropImage;
+import com.theartofdev.edmodo.cropper.CropImageView;
+
+import java.text.SimpleDateFormat;
+
+public class EditActivity extends AppCompatActivity {
+
+    DBHelper helper;
+    EditText TxNama;
+    Spinner TxJenisHidangan;
+    EditText TxBahan;
+    EditText TxLangkah;
+    long id;
+    CircularImageView imageView;
+    Uri uri;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit);
+
+        helper = new DBHelper(this);
+
+        id = getIntent().getLongExtra(DBHelper.row_id, 0);
+
+        TxNama = (EditText)findViewById(R.id.txNama_Edit);
+        TxJenisHidangan = (Spinner)findViewById(R.id.spJw_Edit);
+        TxBahan = (EditText)findViewById(R.id.txAlat_Edit);
+        TxLangkah = (EditText)findViewById(R.id.tx_Langkah_Edit);
+        imageView = (CircularImageView)findViewById(R.id.image_profile);
+
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                CropImage.startPickImageActivity(EditActivity.this);
+            }
+        });
+
+        getData();
+    }
+
+    private void getData(){
+        Cursor cursor = helper.oneData(id);
+        if(cursor.moveToFirst()){
+            String nama = cursor.getString(cursor.getColumnIndex(DBHelper.row_nama));
+            String jenisHidangan = cursor.getString(cursor.getColumnIndex(DBHelper.row_jenis_hidangan));
+            String bahan = cursor.getString(cursor.getColumnIndex(DBHelper.row_bahan));
+            String langkah = cursor.getString(cursor.getColumnIndex(DBHelper.row_langkah));
+            String foto = cursor.getString(cursor.getColumnIndex(DBHelper.row_foto));
+
+            TxNama.setText(nama);
+            if (jenisHidangan.equals("Laki-Laki")){
+                TxJenisHidangan.setSelection(0);
+            }else if(jenisHidangan.equals("Perempuan")){
+                TxJenisHidangan.setSelection(1);
+            }
+            TxBahan.setText(bahan);
+            TxLangkah.setText(langkah);
+
+            if (foto.equals("null")){
+                imageView.setImageResource(R.drawable.iconfood);
+            }else{
+                imageView.setImageURI(Uri.parse(foto));
+            }
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.edit_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.save_edit:
+                String nama = TxNama.getText().toString().trim();
+                String jenisHidangan = TxJenisHidangan.getSelectedItem().toString().trim();
+                String bahan = TxBahan.getText().toString().trim();
+                String langkah = TxLangkah.getText().toString().trim();
+
+                ContentValues values = new ContentValues();
+                values.put(DBHelper.row_nama, nama);
+                values.put(DBHelper.row_jenis_hidangan, jenisHidangan);
+                values.put(DBHelper.row_bahan, bahan);
+                values.put(DBHelper.row_langkah, langkah);
+                values.put(DBHelper.row_foto, String.valueOf(uri));
+
+                if (nama.equals("") || jenisHidangan.equals("")|| bahan.equals("") || langkah.equals("")){
+                    Toast.makeText(EditActivity.this, "Data Tidak Boleh Kosong", Toast.LENGTH_SHORT).show();
+                }else {
+                    helper.updateData(values, id);
+                    Toast.makeText(EditActivity.this, "Data Tersimpan", Toast.LENGTH_SHORT).show();
+                    finish();
+                }
+        }
+        switch (item.getItemId()){
+            case R.id.delete_edit:
+                AlertDialog.Builder builder = new AlertDialog.Builder(EditActivity.this);
+                builder.setMessage("Data ini akan dihapus.");
+                builder.setCancelable(true);
+                builder.setPositiveButton("Hapus", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        helper.deleteData(id);
+                        Toast.makeText(EditActivity.this, "Data Terhapus", Toast.LENGTH_SHORT).show();
+                        finish();
+                    }
+                });
+                builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                });
+                AlertDialog alertDialog = builder.create();
+                alertDialog.show();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @TargetApi(Build.VERSION_CODES.M)
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if (requestCode == CropImage.PICK_IMAGE_CHOOSER_REQUEST_CODE
+                && resultCode == Activity.RESULT_OK){
+            Uri imageuri = CropImage.getPickImageResultUri(this, data);
+            if (CropImage.isReadExternalStoragePermissionsRequired(this, imageuri)){
+                uri = imageuri;
+                requestPermissions(new String[] {Manifest.permission.READ_EXTERNAL_STORAGE}
+                        , 0);
+            }else{
+                startCrop(imageuri);
+            }
+        }
+
+        if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE){
+            CropImage.ActivityResult result = CropImage.getActivityResult(data);
+            if (resultCode == RESULT_OK){
+                imageView.setImageURI(result.getUri());
+                uri = result.getUri();
+            }
+        }
+    }
+
+    private void startCrop(Uri imageuri) {
+        CropImage.activity(imageuri)
+                .setGuidelines(CropImageView.Guidelines.ON)
+                .setAspectRatio(1, 1)
+                .start(this);
+        uri = imageuri;
+    }
+}
